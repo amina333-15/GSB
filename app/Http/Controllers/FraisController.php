5afe26984b5f2a91e247a7b163ae3ff204ad2450
amina333-15 <amina.salmi@lmdsio.fr>
2025-10-02 22:11:09 +0200

    Update FraisController.php

--- app/Http/Controllers/FraisController.php
+++ app/Http/Controllers/FraisController.php
@@ -8,50 +8,62 @@ use App\Models\Frais;
 class FraisController extends Controller
 {
     public function listFrais(){
-        $service = new FraisService();
-        $id_visiteur = session('id_visiteur');
-        $fiches = $service->getListFrais($id_visiteur);
-        return view('listFrais', compact('fiches'));
+        try{
+            $service = new FraisService();
+            $id_visiteur = session('id_visiteur');
+            $fiches = $service->getListFrais($id_visiteur);
+            return view('listFrais', compact('fiches'));
+        } catch (Exception $exception) {
+            return view('error', compact('exception'));
+        }
     }
-
+    
     public function addFrais()
     {
-        $frais = new Frais();
-        $frais->anneemois=date("Y-m");
-
-        return view('formFrais', compact('frais'));
+        try{
+            $frais = new Frais();
+            $frais->anneemois=date("Y-m");
+            return view('formFrais', compact('frais'));
+        } catch (Exception $exception) {
+            return view('error', compact('exception'));
+        }
     }
 
     public function validFrais(Request $request)
     {
-        $id_frais = $request->input('id');
-        $service = new FraisService();
+        try{
+            $id_frais = $request->input('id');
+            $service = new FraisService();
+            if ($id_frais) {
+                $frais = $service->getFrais($id_frais);
+                $frais->id_visiteur = session('id_visiteur');
+            } else {
+                $frais = new Frais();
+                $frais->id_visiteur = session('id_visiteur');
+            }
+            $frais->anneemois = $request->input('mois');
+            $frais->nbjustificatifs = $request->input('nbjustif');
+            $frais->montantvalide = $request->input('valide');
+            $frais->id_etat = $request->input('etat');
+            $frais->datemodification = date('Y-m-d');
 
-        if ($id_frais) {
-            $frais = $service->getFrais($id_frais);
-            $frais->id_visiteur = session('id_visiteur');
-        } else {
-            $frais = new Frais();
-            $frais->id_visiteur = session('id_visiteur');
+            $service = new FraisService();
+            $service->saveFrais($frais);
+            return redirect(url('/listerFrais'));
+        } catch (Exception $exception) {
+            return view('error', compact('exception'));
         }
-
-        $frais->anneemois = $request->input('mois');
-        $frais->nbjustificatifs = $request->input('nbjustif');
-        $frais->montantvalide = $request->input('valide');
-        $frais->id_etat = $request->input('etat');
-        $frais->datemodification = date('Y-m-d');
-
-        $service = new FraisService();
-        $service->saveFrais($frais);
-        return redirect(url('/listerFrais'));
     }
 
 
     public function editFrais($id)
     {
+        try{
             $service = new FraisService();
             $frais = $service->getFrais($id);
             return view('formFrais', compact('frais'));
+        } catch (Exception $exception) {
+            return view('error', compact('exception'));
+        }
     }
 }
-
