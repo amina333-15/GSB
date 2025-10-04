8e1346aa985064b39b7d11a0e4061adedd087179
amina333-15 <amina.salmi@lmdsio.fr>
2025-10-04 17:31:43 +0200

    Update Etat.php

--- app/Models/Etat.php
+++ app/Models/Etat.php
@@ -1,1 +1,13 @@
+<?php
 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Frais extends Model
+{
+    protected $table ='etat';
+    protected $primaryKey = 'id_etat';
+    public $timestamps = false;
+}
