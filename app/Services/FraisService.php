405e529802aa945b3cb6c9303a9a9fbfa0be18ee
asalm <amina.salmi@lmdsio.fr>
2025-10-09 16:01:27 +0200

    V6

--- app/Services/FraisService.php
+++ app/Services/FraisService.php
@@ -9,68 +9,64 @@ use Illuminate\Database\QueryException;
 
 class FraisService
 {
-    public function getListFrais($id_visiteur){
+    public function getListFrais($id_visiteur)
+    {
 
-        try
-        {
+        try {
             $liste = Frais::query()
                 ->select('frais.*', 'etat.lib_etat')
                 ->join('etat', 'etat.id_etat', '=', 'frais.id_etat')
                 ->where('id_visiteur', '=', $id_visiteur)
                 ->get();
-        }
-        catch(QueryException $exception)
-        {
-            $userMessage="Impossible d'accéder à la base de données.";
+        } catch (QueryException $exception) {
+            $userMessage = "Impossible d'accéder à la base de données.";
             throw new UserException($userMessage, $exception->getMessage(), $exception->getCode());
         }
 
         return $liste;
     }
 
-    public function getFrais($id){
-        try
-        {
+    public function getFrais($id)
+    {
+        try {
             $frais = Frais::query()->find($id);
-        }
-        catch(QueryException $exception)
-        {
-            $userMessage="Impossible d'accéder à la base de données.";
+        } catch (QueryException $exception) {
+            $userMessage = "Impossible d'accéder à la base de données.";
             throw new UserException($userMessage, $exception->getMessage(), $exception->getCode());
         }
         return $frais;
     }
 
-    public function saveFrais(Frais $frais){
-        try
-        {
+    public function saveFrais(Frais $frais)
+    {
+        try {
             $frais->save();
-        }
-        catch(QueryException $exception)
-        {
-            $userMessage="Impossible d'accéder à la base de données.";
+        } catch (QueryException $exception) {
+            $userMessage = "Impossible d'accéder à la base de données.";
             throw new UserException($userMessage, $exception->getMessage(), $exception->getCode());
         }
     }
 
-    public function getListEtats(){
-        try{
+    public function getListEtats()
+    {
+        try {
             Etat::query()->get();
-    } catch (QueryException $exception) {
-            $userMessage="Impossible d'accéder à la base de données.";
+        } catch (QueryException $exception) {
+            $userMessage = "Impossible d'accéder à la base de données.";
             throw new UserException($userMessage, $exception->getMessage(), $exception->getCode());
         }
     }
 
-    public function deleteFrais($id){
-        try{
+    public function deleteFrais($id)
+    {
+        try {
             $frais = Frais::query()->find($id);
             $frais->delete();
         } catch (QueryException $exception) {
             if ($exception->getCode() == 23000) {
-                $userMessage="Impossible de supprimer une fiche avec des frais saisis.";
-            }else{
-                $userMessage="Erreur de suppression dans le base de données.";
+                $userMessage = "Impossible de supprimer une fiche avec des frais saisis.";
+            } else {
+                $userMessage = "Erreur de suppression dans le base de données.";
             }
             throw new UserException($userMessage, $exception->getMessage(), $exception->getCode());
         }
