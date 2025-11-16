38f6e64c07d08f2297c65ac83521e991219bd40a
amina333-15 <amina.salmi@lmdsio.fr>
2025-11-16 13:48:40 +0100

    correction

--- app/Services/VisiteurService.php
+++ app/Services/VisiteurService.php
@@ -2,42 +2,25 @@
 
 namespace App\Services;
 
-use App\Exceptions\UserException;
-use Illuminate\Database\QueryException;
 use Illuminate\Support\Facades\Session;
 use App\Models\Visiteur;
-
 
 class VisiteurService
 {
     public function signIn($login, $pwd)
     {
-        try{
-        $visiteur = Visiteur::query()->where('login_visiteur', '=', $login)->first();
+        $visiteur = Visiteur::where('login_visiteur', $login)->first();
         if ($visiteur && $visiteur->pwd_visiteur === $pwd) {
             Session::put('id_visiteur', $visiteur->id_visiteur);
             Session::put('visiteur', "$visiteur->prenom_visiteur $visiteur->nom_visiteur");
             return true;
-        }
-    } catch(QueryException $exception)
-        {
-            $userMessage="Impossible d'accéder à la base de données.";
-            throw new UserException($userMessage, $exception->getMessage(), $exception->getCode());
         }
         return false;
     }
 
     public function signOut()
     {
-        try
-        {
-            Session::remove('id_visiteur');
-        }
-        catch(QueryException $exception)
-        {
-            $userMessage="Impossible d'accéder à la base de données.";
-            throw new UserException($userMessage, $exception->getMessage(), $exception->getCode());
-        }
+        Session::forget('id_visiteur');
+        Session::forget('visiteur');
     }
-
 }
