3d9931c7e19de7904ac529e19478402236fe7e00
amina333-15 <amina.salmi@lmdsio.fr>
2025-11-16 13:46:59 +0100

    correction

--- app/Http/Controllers/VisiteurController.php
+++ app/Http/Controllers/VisiteurController.php
@@ -10,58 +10,27 @@ class VisiteurController extends Controller
 {
     public function login()
     {
-        try{
-            return view('formLogin');
-        }catch (Exception $exception){
-            return view('error', compact('exception'));
-        }
+        return view('formLogin');
     }
-
-//
-//    public function auth(Request $request)
-//    {
-//        $login = $request->input("login");
-//        $pwd = $request->input("pwd");
-//
-//        $service = new VisiteurService();
-//        $visiteur = $service->signIn($login, $pwd);
-//
-//        if ($visiteur && $visiteur->pwd_visiteur === $pwd) {
-//            Session::put('id_visiteur', $visiteur->id_visiteur);
-//            Session::put('visiteur', "{$visiteur->prenom_visiteur} {$visiteur->nom_visiteur}");
-//            return redirect(url('/'));
-//        } else {
-//            $erreur = "Identifiant ou mot de passe incorrect";
-//            return view('formLogin', compact('erreur'));
-//        }
-//    }
-
 
     public function auth(Request $request)
     {
-        try{
-            $login = $request->input("login");
-            $pwd = $request->input("pwd");
+        $login = $request->input("login");
+        $pwd = $request->input("pwd");
 
-            $service = new VisiteurService();
-            if ($service->signIn($login, $pwd)) {
-                return redirect(url('/'));
-            } else {
-                $erreur = "Identifiant ou mot de passe incorrect";
-                return view('/formLogin', compact('erreur'));
-            }
-        }catch (Exception $exception){
-            return view('error', compact('exception'));
+        $service = new VisiteurService();
+        if ($service->signIn($login, $pwd)) {
+            return redirect(url('/'));
+        } else {
+            $erreur = "Identifiant ou mot de passe incorrect";
+            return view('formLogin', compact('erreur'));
         }
     }
 
     public function logout()
     {
-        try {
-            $service = new VisiteurService();
-            $service->signOut();
-            return redirect(url('/'));
-        } catch (Exception $exception) {
-            return view('error', compact('exception'));
-        }
-    }}
+        $service = new VisiteurService();
+        $service->signOut();
+        return redirect(url('/'));
+    }
+}
